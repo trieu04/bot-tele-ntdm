2630ba66e13e010dbabe0162e7752ca515389565
QuocTrieu <quoctrieudev@gmail.com>
2021-07-24 17:39:21 +0700

    Listen

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { Telegraf } = require('telegraf')
+require('http')
+  .createServer(bot.webhookCallback('/secret-path'))
+  .listen(process.env.PORT || 3000)
 
+require('https')
+  .createServer(tlsOptions, bot.webhookCallback('/secret-path'))
+  .listen(process.env.PORT || 8443)
 const bot = new Telegraf('1921157192:AAGID1EpbU-EBGSe8jxrQSQgfKGi69SMvB8')
 bot.listen(process.env.PORT || 5000)
 bot.start((ctx) => ctx.reply('Welcome'))
