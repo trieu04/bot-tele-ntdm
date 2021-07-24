258e9f03ae2fe3e7def73f29bb02d89e12b09df5
QuocTrieu <quoctrieudev@gmail.com>
2021-07-24 17:31:20 +0700

    F

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf('1921157192:AAGID1EpbU-EBGSe8jxrQSQgfKGi69SMvB8')
+bot.listen(process.env.PORT || 5000)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
