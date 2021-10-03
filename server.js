2a3ac1bcb8c7070bbf79fbdd36a23603e7d9bb1a
ntdm11 <ntdm11@gmail.com>
2021-10-03 21:04:57 +0700

    web

--- server.js
+++ server.js
@@ -345,3 +345,16 @@ job.start();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+const express = require('express')
+// ===Your bot logic here 
+
+// Start app for Heroku
+const app = express()
+app.use(express.static('public'))
+app.get('/', function (req, res) {
+  res.send( "QuocTrieuIT" )
+})
+
+// Start server
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
