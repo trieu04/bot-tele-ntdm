8f2afb426eeba552e9dc7dd64afbe73bd2f01b39
ntdm11 <ntdm11@gmail.com>
2021-10-03 21:26:16 +0700

    web s

--- server.js
+++ server.js
@@ -345,7 +345,7 @@ job.start();
 const PORT = process.env.PORT || 3000;
 
 bot.telegram.setWebhook('https://bot-tele-ntdm.herokuapp.com/telegram');
-bot.startWebhook('/telegram', null, PORT)
+bot.startWebhook('/telegram', null, 5000)
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
