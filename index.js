908ddbc5cf9a17f904a7c77ac2a3c56e6dfca20d
ntdm11 <ntdm11@gmail.com>
2021-10-03 21:00:38 +0700

    web

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf('1965412655:AAGKqyIE20dI6H0KrNcZ8pNUhWUch2y9yCI')
+const express = require('express')
+// ===Your bot logic here 
 
+// Start app for Heroku
+const app = express()
+app.use(express.static('public'))
+app.get('/', function (req, res) {
+  res.send( "QuocTrieuIT" )
+})
 
-
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
-
-require('http')
-  .createServer(bot.webhookCallback('/secret-path'))
-  .listen(process.env.PORT || 3000)
+// Start server
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
