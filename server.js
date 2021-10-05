db9d473ac250490866f9eeb743f5d340f3571ab2
ntdm11 <ntdm11@gmail.com>
2021-10-05 23:18:39 +0700

    Procfile

--- server.js
+++ server.js
@@ -323,13 +323,13 @@ function secondsToDhms(seconds, short = true) {
         var dDisplay = d > 0 ? d + "d " : "";
         var hDisplay = h > 0 ? h + "h " : "";
         var mDisplay = m > 0 ? m + "m " : "";
-        var sDisplay = s > 0 ? s + "s" : "";
+        var sDisplay = s + "s"
     }
     else {
         var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
         var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
         var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+        var sDisplay = s + (s <= 1 ? " second" : " seconds");
     }
 
     return dDisplay + hDisplay + mDisplay + sDisplay;
