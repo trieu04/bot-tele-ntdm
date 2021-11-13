b3201de4784e43563fbe491d045c539511b2e3cd
QuocTrieuDev <ngamthoduoimua@gmail.com>
2021-11-13 12:34:14 +0700

    New File

---
+++ modules/mysql.js
@@ -0,0 +1,29 @@
+const process = require('process');
+var events = require('events');
+const CronJob = require('cron').CronJob;
+
+require('dotenv').config();
+const os = require("os");
+
+const axios = require('axios').default;
+const fs = require('fs');
+var progress = require('progress-stream');
+const search = require('youtube-search');
+const ytdl = require('ytdl-core');
+
+const mysql = require('mysql')
+
+var mysqlc = mysql.createConnection({
+  host     : 'free01.123host.vn',
+  user     : 'quoctrie',
+  password : 'free01.123host.vn',
+  database : 'quoctrie_faye'
+})
+mysqlc.connect();
+
+mysqlc.query('SELECT * FROM yt-media', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+
+
--- tmp.js
+++
@@ -1,2 +0,0 @@
-var str = "abc"
-console.log( EOL);
