8d447b9a0ff969ce16caf299af94c9dd88b9284c
trieu04 <truongquoctrieu04@gmail.com>
2023-06-11 06:39:43 +0700

    Update makeup.js
    add input check
    return null if input is not a string

--- includes/utils/makeup.js
+++ includes/utils/makeup.js
@@ -1,4 +1,6 @@
 const htmlEscape = (unsafe) => {
+    if(typeof unsafe != "string")
+        return null
     return unsafe
         .replaceAll('&', '&amp;')
         .replaceAll('<', '&lt;')
@@ -8,30 +10,48 @@ const htmlEscape = (unsafe) => {
 }
 const HTMLMakup =  {
     bold(t){
+        if(typeof t != "string")
+            return null
         return '<b>' + htmlEscape(t) + '</b>'  
     },
     italic(t){
+        if(typeof t != "string")
+        return null
         return '<i>' + htmlEscape(t) + '</i>' 
     },
     strike(t){
+        if(typeof t != "string")
+            return null
         return '<s>' + htmlEscape(t) + '</s>' 
     },
     underline(t){
+        if(typeof t != "string")
+            return null
         return '<u>' + htmlEscape(t) + '</u>' 
     },
     spoiler(t){
+        if(typeof t != "string")
+            return null
         return '<span class="tg-spoiler">' + t + '</span>'
     },
     code(t){
+        if(typeof t != "string")
+            return null
         return '<code>' + htmlEscape(t) + '</code>' 
     },
     link(link, t){
+        if(typeof t != "string")
+            return null
         return `<a href="${encodeURIComponent(link)}>${t}</a>"`
     },
     pre(language, t){
+        if(typeof t != "string")
+            return null
         return `<pre language="${encodeURIComponent(language)}>${t}</pre>"`
     },
     makeup(format, t){
+        if(typeof t != "string")
+            return null
         t = htmlEscape(t)
         for(c of format){
             if(! c in ["b", "i", "s", "u"]) continue;
